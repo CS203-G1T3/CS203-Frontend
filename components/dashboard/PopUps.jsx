85d0f643916a03bd743e250e9c8dac0cae81cfcd
tayfaith <tayfaithse@gmail.com>
2021-09-27 22:50:57 +0800

    fixed modals

--- components/dashboard/PopUps.jsx
+++ components/dashboard/PopUps.jsx
@@ -1,7 +1,7 @@
 import React, {useState} from "react";
 import {Modal, Button} from "antd";
 
-const PopUps = () => {
+const PopUps = ({ header, value, title, body, disclaimer}) => {
     const [isModalVisible, setIsModalVisible] = useState(false);
 
     const showModal = () => {
@@ -10,27 +10,39 @@ const PopUps = () => {
 
     const handleOk = () =>{
         setIsModalVisible(false);
+        
     }
 
     const handleCancel = () => {
         setIsModalVisible(false);
-    }
-
+      };
+    
     return(
-        <>
-        <button type="primary" onClick={showModal}>
-            Open
-        </button>
-        <Modal
-            title = "Basic Modal"
-            visible={isModalVisible}
-            onOk={handleOk}
-            onCancel={handleCancel}
-        >
-        
-        </Modal>
-        </>
+        <div className="flex flex-col items-baseline shadow-xl h-50 w-60 m-4 bg-gray-200 rounded-lg p-4" id="open-btn">
+            <p className="order-first">{header}</p>
+            <p className="order-second text-2xl font-bold">{value}</p>
+            <button type="primary" onClick={showModal}>
+                Click here for more details
+            </button>
+            <Modal
+                title={title}
+                visible={isModalVisible}
+                onOk={handleOk}
+                okText={<a href = "https://www.stb.gov.sg/content/stb/en/home-pages/advisory-for-food-and-beverage-establishments.html" target="_blank">READ MORE</a>}
+                okType="round"
+                onCancel={handleCancel}
+                cancelButtonProps={{style:{display:'none'}}}
+            >
+                <p className="text-2xl">Safe Management Measures for F&B Establishments - Coffee Shops</p>
+                <p className="text-blue-600">Updated as of 27 July 2021</p>
+                <p className ="text-900">{body}</p>
+                <p className="text-gray-600">Safe Management Measures are mandated by the Ministry of Manpower (MOM) and the Multi Ministry Task Force.</p>
+            </Modal>
+
+        </div>
+    
     );
 }
 
 export default PopUps;
+
--- pages/dashboard/index.jsx
+++ pages/dashboard/index.jsx
@@ -1,7 +1,6 @@
 import Navbar from "../../components/dashboard/Navbar"
 import UserProfile from "../../components/dashboard/UserProfile"
 import { SearchIcon } from '@heroicons/react/solid';
-import DashboardCard from "../../components/dashboard/DashboardCard";
 import PopUps from "../../components/dashboard/PopUps"
 
 function Dashboard () {
@@ -9,7 +8,6 @@ function Dashboard () {
     return (
         <div className="h-screen flex">
             <Navbar />
-            <PopUps />
             
             <div className="p-4 w-full">
                 {/* search bar and language */}
@@ -33,12 +31,13 @@ function Dashboard () {
 
                 <div class="grid grid-flow-col lg:grid-cols-3 lg:grid-rows-2 gap-4 md:grid-cols-2 md:grid-rows-3">
 
-                    <DashboardCard header="Allowed To Operate?" value="YES" link="#" />
-                    <DashboardCard header="Contact Tracing" value="100% required SAFE ENTRY" link="#" />
-                    <DashboardCard header="Group Size" value="2 PAX" link="#" />
-                    <DashboardCard header="Operating Capacity" value="100%" link="#" />
-                    <DashboardCard header="COVID-19 Swab Test Frequency" value="Every 14 Days" link="#" />
-                    <DashboardCard header="Operating Guidelines" value="By MOM" link="#" />
+                    
+                    <PopUps header="Allowed To Operate?" value="YES" title="Allowed To Operate" body="F&B establishments are only permitted to seat dine-in groups of up to 2 persons, i.e. a decrease from groups of up to 5 persons allowed previously. Vaccination-differentiated SMMs must continue to be applied to such dine-in groups or individuals."/>
+                    <PopUps header="Contact Tracing" value="100% required SAFE ENTRY" title="100% required SAFE ENTRY" body=" F&B establishments must implement SafeEntry via TraceTogether-only SafeEntry (TT-only SE) for dine-in customers and visitors"/>
+                    <PopUps header="Group Size" value="2 PAX" title="Group Size: 2 Pax" body="As hawker centres and coffeeshops are open-air and naturally ventilated spaces, a special concession would be given to allow vaccinated and unvaccinated individuals to dine in these settings, but subject to group sizes of up to 2 persons only."/>
+                    <PopUps header="Operating Capacity" value="100%" title="Operating Capacity Determined By Layout" body="Where tables/seats are fixed, tables/seats should be marked out to accommodate groups of no more than 2 persons, while ensuring at least one-metre spacing between groups."/>
+                    <PopUps header="COVID-19 Swab Test Frequency" value="Every 7 Days" title="Swab Test Required Every 7 Days" body="All individuals working at F&B establishments providing dine-in services (including part-time and full-time employees, third-party contracted staff must undergo testing once every 7 days using tests such as the antigen rapid test (ART) regardless of their vaccination status, under the regular Fast and Easy Test Rostered Routine Testing (FET RRT) regime. Individuals who have recovered from a COVID-19 infection in the past 270 days are exempted."/>
+                    <PopUps header="Operating Guidelines" value="By MOM" title="Operating Guidelines" body=" F&B establishments must ensure that a safe distance of at least one metre is maintained between groups of up to 2 customers to mitigate the risk of transmission. This refers to the distance between the edges of every group or person. In addition, F&B establishments must ensure that the furniture is arranged in such a way to facilitate safe distancing between groups – for example, the distance measured between the backs of chairs used by customers in different groups, or the legs of chairs if there is no back, must also be at least one metre apart"/>
 
                 </div>
 
